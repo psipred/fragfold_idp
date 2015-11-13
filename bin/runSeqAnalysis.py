b8e8ad31f8b606308d761a9c21c5871eefeeabe1
Daniel Buchan <clone@myself.com>
2015-11-13 16:28:12 +0000

    skeleton of argument parsing added

--- bin/runSeqAnalysis.py
+++ bin/runSeqAnalysis.py
@@ -1,4 +1,18 @@
+import sys
+import argparse
+
 """
     1st part of the process. Python script takes a sequence then
     runs PSIPRED and HHBlits and output MSA and SS for Fragfold
 """
+parser = argparse.ArgumentParser(description='Runs the preliminary sequence '
+                                             'parser for FF-IDP')
+
+parser.add_argument('--input', help="input fasta file")
+
+args = parser.parse_args()
+
+try:
+    fasta_file = sys.argv[1]
+except:
+    parser.print_help()
